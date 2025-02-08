3ae0c1a07476ab3959982583f6d5b117944ee234
mimshins <mostafa.sh.coderino@gmail.com>
2025-02-08 11:20:34 +0330

    refactor(container): update the abstract interface

--- src/Container.ts
+++ src/Container.ts
@@ -1,7 +1,10 @@
+import { Entity } from "./constants.ts";
 import type {
   CallableFunction,
+  ComputedEntity,
   ComputeValue,
   ContainerEntity,
+  DefaultEntity,
   EqualityCheckFunction,
   Initializer,
   SubscribeCallback,
@@ -25,14 +28,20 @@ abstract class Container<T> {
   /**
    * A snapshot of the state.
    */
-  public abstract get value(): T;
-  public abstract set value(newValue: T);
+  public getValue(): T {
+    return this._value;
+  }
+
+  /**
+   * Updates the value of the state and notifies the subscribers.
+   */
+  public abstract setValue(newValue: T): void | Promise<void>;
 
   /**
    * Subscribes to the changes of the container's state value
    * and returns the unsubscribe function.
    */
-  public abstract subscribe(
+  public subscribe(
     cb: SubscribeCallback<T>,
     options?: {
       /**
@@ -40,7 +49,27 @@ abstract class Container<T> {
        */
       signal?: AbortSignal;
     },
-  ): Unsubscribe;
+  ): Unsubscribe {
+    const { signal } = options ?? {};
+
+    const entity: DefaultEntity<T> = {
+      type: Entity.DEFAULT,
+      cb,
+    };
+
+    this._subscribers.add(entity);
+
+    const unsubscribe: Unsubscribe = () => {
+      this._subscribers.delete(entity);
+      signal?.removeEventListener("abort", unsubscribe);
+    };
+
+    signal?.addEventListener("abort", unsubscribe);
+
+    if (signal?.aborted) unsubscribe();
+
+    return unsubscribe;
+  }
 
   /**
    * Subscribes to the changes of the container's selected state values
@@ -48,7 +77,7 @@ abstract class Container<T> {
    *
    * For more control over emission changes, you may provide a custom equality function.
    */
-  public abstract computedSubscribe<P>(
+  public computedSubscribe<P>(
     computeValue: ComputeValue<T, P>,
     cb: SubscribeCallback<P>,
     options?: {
@@ -61,7 +90,29 @@ abstract class Container<T> {
        */
       isEqual?: EqualityCheckFunction<P>;
     },
-  ): Unsubscribe;
+  ): Unsubscribe {
+    const { isEqual, signal } = options ?? {};
+
+    const entity: ComputedEntity<T, P> = {
+      type: Entity.COMPUTED,
+      computeValue,
+      cb,
+      isEqual,
+    };
+
+    this._subscribers.add(entity);
+
+    const unsubscribe: Unsubscribe = () => {
+      this._subscribers.delete(entity);
+      signal?.removeEventListener("abort", unsubscribe);
+    };
+
+    signal?.addEventListener("abort", unsubscribe);
+
+    if (signal?.aborted) unsubscribe();
+
+    return unsubscribe;
+  }
 }
 
 export default Container;
--- src/StateContainer.ts
+++ src/StateContainer.ts
@@ -1,25 +1,13 @@
 import { Entity } from "./constants.ts";
 import Container from "./Container.ts";
-import type {
-  ComputedEntity,
-  ComputeValue,
-  DefaultEntity,
-  EqualityCheckFunction,
-  Initializer,
-  SubscribeCallback,
-  Unsubscribe,
-} from "./types";
+import type { Initializer } from "./types";
 
 class StateContainer<T> extends Container<T> {
   static create<T>(initializer: Initializer<T>): StateContainer<T> {
     return new StateContainer(initializer);
   }
 
-  public get value(): T {
-    return this._value;
-  }
-
-  public set value(newValue: T) {
+  public setValue(newValue: T): void {
     const prevValue = this._value;
 
     this._value = newValue;
@@ -45,64 +33,6 @@ class StateContainer<T> extends Container<T> {
         cb(computedValue);
       }
     });
-  }
-
-  public subscribe(
-    cb: SubscribeCallback<T>,
-    options?: {
-      signal?: AbortSignal;
-    },
-  ): Unsubscribe {
-    const { signal } = options ?? {};
-
-    const entity: DefaultEntity<T> = {
-      type: Entity.DEFAULT,
-      cb,
-    };
-
-    this._subscribers.add(entity);
-
-    const unsubscribe: Unsubscribe = () => {
-      this._subscribers.delete(entity);
-      signal?.removeEventListener("abort", unsubscribe);
-    };
-
-    signal?.addEventListener("abort", unsubscribe);
-
-    if (signal?.aborted) unsubscribe();
-
-    return unsubscribe;
-  }
-
-  public computedSubscribe<P>(
-    computeValue: ComputeValue<T, P>,
-    cb: SubscribeCallback<P>,
-    options?: {
-      signal?: AbortSignal;
-      isEqual?: EqualityCheckFunction<P>;
-    },
-  ): Unsubscribe {
-    const { isEqual, signal } = options ?? {};
-
-    const entity: ComputedEntity<T, P> = {
-      type: Entity.COMPUTED,
-      computeValue,
-      cb,
-      isEqual,
-    };
-
-    this._subscribers.add(entity);
-
-    const unsubscribe: Unsubscribe = () => {
-      this._subscribers.delete(entity);
-      signal?.removeEventListener("abort", unsubscribe);
-    };
-
-    signal?.addEventListener("abort", unsubscribe);
-
-    if (signal?.aborted) unsubscribe();
-
-    return unsubscribe;
   }
 }
 
