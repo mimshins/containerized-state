f85b57b0e247260089ef6d94d1b2479a4c6b6f8a
mimshins <mostafa.sh.coderino@gmail.com>
2025-02-08 11:44:41 +0330

    feat: expose public api

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { default as AsyncStateContainer } from "./AsyncStateContainer.ts";
+export { default as StateContainer } from "./StateContainer.ts";
+export type {
+  ComputeValue,
+  EqualityCheckFunction,
+  Initializer,
+  SubscribeCallback,
+  Unsubscribe,
+} from "./types.ts";
