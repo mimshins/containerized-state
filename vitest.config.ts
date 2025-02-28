0cdbbccc8a81c97310b5dc9d2de18a38dbcebd21
mimshins <mostafa.sh.coderino@gmail.com>
2025-02-28 21:31:06 +0330

    chore(test): add v8 coverage reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,23 @@ const config = defineConfig({
   test: {
     environment: "node",
     include: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "clover", "json-summary"],
+      include: ["src/**"],
+      exclude: [
+        "src/Container.ts",
+        "src/index.ts",
+        "src/types.ts",
+        "coverage/**",
+        "dist/**",
+        "**\/*.d.ts",
+        "test?(s)/**",
+        "test?(-*).?(c|m)[jt]s?(x)",
+        "**\/*{.,-}{test,spec}?(-d).?(c|m)[jt]s?(x)",
+        "**\/__tests__/**",
+      ],
+    },
   },
 });
 
